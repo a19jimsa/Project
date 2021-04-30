9c1b44104cc9b84f5596ec7a2b7e16983d180ce1
a19jimsa <a19jimsa@student.his.se>
2021-04-30 17:51:15 +0200

    Added RecyclerViewItem Added RecyclerViewAdapter Added items to item list

---
+++ app/src/main/java/com/example/project/RecyclerViewItem.java
@@ -0,0 +1,60 @@
+package com.example.project;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+@SuppressWarnings("WeakerAccess")
+public class RecyclerViewItem{
+    @SerializedName("ID")
+    private String id;
+    private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
+    private int size;
+    private int cost;
+    private String auxdata;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String getAuxdata() {
+        return auxdata;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}

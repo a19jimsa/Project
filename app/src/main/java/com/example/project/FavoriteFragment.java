c9762b9f2afc8e57d85ff37e21a1a473510f11b5
a19jimsa <a19jimsa@student.his.se>
2021-05-10 12:23:46 +0200

    Added sort variable to be able to change sort of favorite list. And Some minor recommended changes from IDE. Added sort icon of button.

--- app/src/main/java/com/example/project/FavoriteFragment.java
+++ app/src/main/java/com/example/project/FavoriteFragment.java
@@ -25,16 +25,17 @@ import java.util.List;
 
 import static android.content.Context.MODE_PRIVATE;
 import static com.example.project.DatabaseTables.Quiz.COLUMN_NAME_CATEGORY;
+import static com.example.project.DatabaseTables.Quiz.COLUMN_NAME_NAME;
 import static com.example.project.DatabaseTables.Quiz.TABLE_NAME;
 
 public class FavoriteFragment extends Fragment {
     private List<RecyclerViewItem> list;
-    private RecyclerViewItem [] items;
-    private ArrayAdapter<RecyclerViewItem> adapter;
+    private final RecyclerViewItem [] items;
     private SQLiteDatabase database;
     private DatabaseHelper databaseHelper;
     private String selection;
     private String [] selectionArgs;
+    private String sort;
     private ListView listView;
     private SharedPreferences myPreferenceRef;
     private SharedPreferences.Editor myPreferenceEditor;
@@ -54,6 +55,7 @@ public class FavoriteFragment extends Fragment {
         myPreferenceEditor = myPreferenceRef.edit();
         selection = COLUMN_NAME_CATEGORY + "= ?";
         selectionArgs = new String[]{myPreferenceRef.getString("category", "Novalue")};
+        sort = myPreferenceRef.getString("sort", "ASC");
         addQuiz();
     }
 
@@ -88,8 +90,8 @@ public class FavoriteFragment extends Fragment {
         }
     }
 
-    private List<RecyclerViewItem> getQuiz(String selection, String [] selectionArgs) {
-        Cursor cursor = database.query(TABLE_NAME, null, selection, selectionArgs, null, null, COLUMN_NAME_CATEGORY + " DESC");
+    private List<RecyclerViewItem> getQuiz() {
+        Cursor cursor = database.query(TABLE_NAME, null, selection, selectionArgs, null, null, COLUMN_NAME_NAME + " " + sort);
         List<RecyclerViewItem> quizList = new ArrayList<>();
         while (cursor.moveToNext()) {
             RecyclerViewItem quiz = new RecyclerViewItem(
@@ -131,21 +133,31 @@ public class FavoriteFragment extends Fragment {
                         savePref();
                         updateAdapter();
                         break;
+                    case R.id.sort_item:
+                        if(sort.contains("ASC")){
+                            sort = "DESC";
+                        }else{
+                            sort = "ASC";
+                        }
+                        savePref();
+                        updateAdapter();
+                        break;
                 }
                 return true;
         }
     };
 
     private void updateAdapter(){
-        list = getQuiz(selection, selectionArgs);
+        list = getQuiz();
         //In order to update the listview.
-        adapter = new ArrayAdapter<>(getContext(), R.layout.favorite_item_list, R.id.favorite_textView, list);
+        ArrayAdapter<RecyclerViewItem> adapter = new ArrayAdapter<>(getContext(), R.layout.favorite_item_list, R.id.favorite_textView, list);
         listView.setAdapter(adapter);
     }
 
     private void savePref(){
         // Store the new preference
         myPreferenceEditor.putString("category", selectionArgs[0]);
+        myPreferenceEditor.putString("sort", sort);
         myPreferenceEditor.apply();
     }
 }
